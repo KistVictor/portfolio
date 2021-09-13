6c451b60843c432625ab85fac574a322305d12d4
Victor Kist <victoraugustokist@gmail.com>
2021-09-13 09:50:20 -0300

    netlify error, trying again

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import FrontPage from "../components/FrontPage/Index"
 import About from "../components/Informations/Index"
-import Works from "../components/Works/Index"
 
 export default function Home() {
     return(
         <>
             <FrontPage/>
             <About/>
-            {/*<Works/>*/}
         </>
     )
 }
